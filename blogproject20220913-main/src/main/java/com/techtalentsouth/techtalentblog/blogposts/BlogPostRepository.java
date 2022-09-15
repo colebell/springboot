8c7a0e739c434c94872f24811068f4b9ad45b21a
colebell <56887506+colebell@users.noreply.github.com>
2022-09-14 21:56:20 -0700

    Delete blogproject20220913-main directory

--- blogproject20220913-main/src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techtalentsouth.techtalentblog.blogposts;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
-
-}
