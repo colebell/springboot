90266f541751253fa8cbbe7dfa88d5aad6a849ea
colebell <56887506+colebell@users.noreply.github.com>
2022-09-19 21:05:23 -0700

    Update BlogPostRepository.java

--- blogproject/src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostRepository.java
+++ blogproject/src/main/java/com/techtalentsouth/techtalentblog/blogposts/BlogPostRepository.java
@@ -1,9 +1,13 @@
 package com.techtalentsouth.techtalentblog.blogposts;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
+//cole
 @Repository
 public interface BlogPostRepository extends CrudRepository<BlogPost, Long> {
-
+	@Override
+	public List<BlogPost> findAll(); 
+	
 }
